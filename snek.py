3a45e43be2347c107f7ec16af3d2a409bca923ec
Manu Zarraga <manuzarraga@hotmail.com>
2023-02-24 12:20:44 -0300

    adding files

---
+++ snek.py
@@ -0,0 +1,153 @@
+import turtle
+import time
+import random
+ 
+delay = 0.1
+score = 0
+high_score = 0
+ 
+ 
+# Window Screen
+wn = turtle.Screen()
+wn.title("Ekans Game")
+wn.bgcolor("black")
+wn.setup(width=600, height=600)
+wn.tracer(0)
+ 
+# Head
+head = turtle.Turtle()
+head.shape("square")
+head.color("white")
+head.penup()
+head.goto(0, 0)
+head.direction = "Stop"
+ 
+# Food
+food = turtle.Turtle()
+colors = ('red')
+shapes = ('circle')
+food.speed(0)
+food.shape(shapes)
+food.color(colors)
+food.penup()
+food.goto(0, 100)
+ 
+pen = turtle.Turtle()
+pen.speed(0)
+pen.shape("square")
+pen.color("white")
+pen.penup()
+pen.hideturtle()
+pen.goto(0, 250)
+ 
+# Key Directions
+def group():
+    if head.direction != "down":
+        head.direction = "up"
+ 
+ 
+def godown():
+    if head.direction != "up":
+        head.direction = "down"
+ 
+ 
+def goleft():
+    if head.direction != "right":
+        head.direction = "left"
+ 
+ 
+def goright():
+    if head.direction != "left":
+        head.direction = "right"
+ 
+ 
+def move():
+    if head.direction == "up":
+        y = head.ycor()
+        head.sety(y+20)
+    if head.direction == "down":
+        y = head.ycor()
+        head.sety(y-20)
+    if head.direction == "left":
+        x = head.xcor()
+        head.setx(x-20)
+    if head.direction == "right":
+        x = head.xcor()
+        head.setx(x+20)
+ 
+ 
+wn.listen()
+wn.onkeypress(group, "w")
+wn.onkeypress(godown, "s")
+wn.onkeypress(goleft, "a")
+wn.onkeypress(goright, "d")
+ 
+segments = []
+ 
+ 
+# Main Gameplay
+while True:
+    pen.write("Score : {} High Score : {} ".format(
+            score, high_score), align="center", font=("roboto", 24, "bold"))
+    wn.update()
+    if head.xcor() > 290 or head.xcor() < -290 or head.ycor() > 290 or head.ycor() < -290:
+        time.sleep(1)
+        head.goto(0, 0)
+        head.direction = "Stop"
+        colors = ('green')
+        shapes = ('square')
+        for segment in segments:
+            segment.goto(1000, 1000)
+        segments.clear()
+        pen.clear()
+        score = 0
+        delay = 0.1
+
+
+    if head.distance(food) < 20:
+        x = random.randint(-270, 270)
+        y = random.randint(-270, 270)
+        food.goto(x, y)
+ 
+        new_segment = turtle.Turtle()
+        new_segment.speed(0)
+        new_segment.shape("square")
+        new_segment.color("green") 
+        new_segment.penup()
+        segments.append(new_segment)
+        delay -= 0.001
+        score += 10
+        if score > high_score:
+            high_score = score
+        pen.clear()
+
+        
+    # Checking Head collisions with body
+    for index in range(len(segments)-1, 0, -1):
+        x = segments[index-1].xcor()
+        y = segments[index-1].ycor()
+        segments[index].goto(x, y)
+    if len(segments) > 0:
+        x = head.xcor()
+        y = head.ycor()
+        segments[0].goto(x, y)
+        
+    move()
+    for segment in segments:
+        if segment.distance(head) < 20:
+            time.sleep(1)
+            head.goto(0, 0)
+            head.direction = "stop"
+
+            colors = ('green')
+            shapes = ('square')
+            for segment in segments:
+                segment.goto(1000, 1000)
+            segment.clear()
+            score = 0
+            delay = 0.1
+            
+            # pen.write("Score : {} High Score : {} ".format(score, high_score), align="center", font=("roboto", 24, "bold"))
+    time.sleep(delay)
+ 
+wn.mainloop()
